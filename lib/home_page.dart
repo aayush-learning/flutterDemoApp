aafa05d3d6dbaa575729f402bc1108e4a64c75e4
Aayush <chimple.aayush@gmail.com>
2020-07-29 01:08:18 +0530

    added http call for ffetching data chapter 8

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:testing_app_flutter/change_name_card.dart';
 import 'package:testing_app_flutter/drawer.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 class HomePage extends StatefulWidget{
@@ -11,10 +13,20 @@ class HomePage extends StatefulWidget{
 class _HomePageState extends State<HomePage>{
   TextEditingController _nameController = TextEditingController();
   var myText = "Change me";
+  var url = "http://jsonplaceholder.typicode.com/photos";
+  var data;
 
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  getData() async{
+    var res = await http.get(url);
+    data = jsonDecode(res.body); 
+    setState(() {
+    });
   }
 
 
@@ -28,9 +40,22 @@ class _HomePageState extends State<HomePage>{
           ),
           body: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-              child: changeNameCard,
-        ),
+            child: data != null ? ListView.builder(
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text(data[index]["title"]),
+                  subtitle: Text("ID: ${data[index]["id"]}"),
+                  leading: Image.network(data[index]["url"])
+                ),
+              ) , 
+              itemCount: data.length) 
+              : 
+              Center(
+                child: CircularProgressIndicator(
+                  
+                ),
+              ) ,
       ),
       drawer: MyDrawer(),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
